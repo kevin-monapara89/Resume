842f1b62e5d268e75df27534e50cd773a3fbab48
kevin monapara <kevinmonapara89@gmail.com>
2023-03-02 21:09:54 +0530

    commit2

--- app/src/main/java/com/kevin/resume/Activity2.java
+++ app/src/main/java/com/kevin/resume/Activity2.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
@@ -14,15 +15,36 @@ public class Activity2 extends AppCompatActivity {
         setContentView(R.layout.activity_2);
 
         TextView btnnext1;
+        EditText edtname, edtaddress, edtemail, edtphone;
 
         btnnext1 = findViewById(R.id.btnnext1);
+        edtname = findViewById(R.id.edtname);
+        edtaddress = findViewById(R.id.edtaddress);
+        edtemail = findViewById(R.id.edtemail);
+        edtphone = findViewById(R.id.edtphone);
 
         btnnext1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Intent intent = new Intent(Activity2.this, Activity3.class);
-                startActivity(intent);
+                String name = edtname.getText().toString();
+                String address = edtaddress.getText().toString();
+                String email = edtemail.getText().toString();
+                String phone = edtphone.getText().toString();
+
+                if (name.isEmpty()) {
+                    edtname.setError("Enter Name!");
+                } else if (address.isEmpty()) {
+                    edtaddress.setError("Enter address!");
+                } else if (email.isEmpty()) {
+                    edtemail.setError("Enter Email!");
+                } else if (phone.isEmpty()) {
+                    edtphone.setError("Enter Phone Number!");
+                } else {
+                    Intent intent = new Intent(Activity2.this, Activity3.class);
+                    intent.putExtra("name", name);
+                    startActivity(intent);
+                }
             }
         });
     }
--- app/src/main/java/com/kevin/resume/Activity3.java
+++ app/src/main/java/com/kevin/resume/Activity3.java
@@ -1,7 +1,11 @@
 package com.kevin.resume;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Activity3 extends AppCompatActivity {
 
@@ -9,5 +13,39 @@ public class Activity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3);
+
+        TextView btnnext2;
+        EditText edtcompany, edtexp, edtjob, edtposs;
+
+        btnnext2 = findViewById(R.id.btnnext2);
+        edtcompany = findViewById(R.id.edtcompany);
+        edtexp = findViewById(R.id.edtexp);
+        edtjob = findViewById(R.id.edtjob);
+        edtposs = findViewById(R.id.edtposs);
+
+        btnnext2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String company = edtcompany.getText().toString();
+                String experiance = edtexp.getText().toString();
+                String job = edtjob.getText().toString();
+                String possition = edtposs.getText().toString();
+
+                if (company.isEmpty()) {
+                    edtcompany.setError("Enter Company Name!");
+                } else if (experiance.isEmpty()) {
+                    edtexp.setError("Enter Experiance!");
+                } else if (job.isEmpty()) {
+                    edtjob.setError("Enter Job Type!");
+                } else if (possition.isEmpty()) {
+                    edtposs.setError("Enter Possition!");
+                } else {
+                    Intent intent = new Intent(Activity3.this, Activity4.class);
+                    intent.putExtra("company", company);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
