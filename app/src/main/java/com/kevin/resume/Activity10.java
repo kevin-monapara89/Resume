3ff966856dc581bfef1f6154f73ac0015da8815a
kevin-monapara89 <kevinmonapara89@gmail.com>
2023-03-09 11:57:44 -0500

    commit5

--- app/src/main/java/com/kevin/resume/Activity10.java
+++ app/src/main/java/com/kevin/resume/Activity10.java
@@ -1,13 +1,38 @@
 package com.kevin.resume;
 
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Activity10 extends AppCompatActivity {
 
+    TextView txtname1, txtphone1, txtemail1, txtaddress1;
+
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_10);
+
+        preferences = getSharedPreferences("data", 0);
+
+        txtname1 = findViewById(R.id.txtname1);
+        String name = preferences.getString("name", " ");
+        txtaddress1 = findViewById(R.id.txtaddress1);
+        String address = preferences.getString("address", " ");
+        txtphone1 = findViewById(R.id.txtphone1);
+        String phone = preferences.getString("phone", " ");
+        txtemail1 = findViewById(R.id.txtemail1);
+        String email = preferences.getString("email", " ");
+
+        txtname1.setText(name);
+        txtaddress1.setText(address);
+        txtphone1.setText(phone);
+        txtemail1.setText(email);
     }
 }
