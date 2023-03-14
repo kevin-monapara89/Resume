f6d401e502b7643d7066b27d8dbc4b25a14a359b
kevin-monapara89 <kevinmonapara89@gmail.com>
2023-03-14 12:52:33 -0400

    commit

--- app/src/main/java/com/kevin/resume/SplashActivity.java
+++ app/src/main/java/com/kevin/resume/SplashActivity.java
@@ -1,18 +1,24 @@
 package com.kevin.resume;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
 
+    ImageView logo;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-
+        logo = findViewById(R.id.logo);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -20,5 +26,9 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         }, 3000);
+
+        Animation animation = AnimationUtils.loadAnimation(SplashActivity.this,R.anim.anime);
+        logo.startAnimation(animation);
+
     }
 }
